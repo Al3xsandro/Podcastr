8866911e4994597a92a35ae48c4ecd89b62445ba
Alex <alexs123655@gmail.com>
2021-05-13 23:11:09 -0300

    create constraint to delete episodes

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@ CREATE TABLE files(
   url varchar(255),
   type varchar(255),
   duration int,
-  FOREIGN KEY (file) REFERENCES podcast(id)
+  FOREIGN KEY (file) REFERENCES podcast(id),
 );
 
 CREATE TABLE podcast(
@@ -13,7 +13,12 @@ CREATE TABLE podcast(
   thumbnail varchar(255),
   description varchar(255),
   published_at DATE,
-  slog varchar(255)
+  slog varchar(255),
+  CONSTRAINT fk_files
+    FOREIGN KEY(file)
+      REFERENCES podcast(id)
+      ON DELETE CASCADE
+
 );
 
 ALTER TABLE files
